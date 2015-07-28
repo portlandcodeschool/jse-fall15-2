db81dab4a3df98316ad220a8d2b71f883fb6e260
sarah Kelleher <sarahkelleher234@gmail.com>
2015-07-27 17:34:20 -0700

    homework

---
+++ sarah/homework2.js
@@ -0,0 +1,38 @@
+homework2.js
+
+1. function fractionstring('n','d'); {
+	var n; 9
+	var d; 7
+	var remainder = n % d
+	var whole number = ((n-remainder/d);
+		var result = whole number + " "+ remainder + '/'+ d;)
+		return result;	
+}
+
+2a. var i = Math.round(n)
+
+2b. for (var i = 0; i > 0; i--) {
+	return (i);
+}
+
+2c. var i = 0;
+	for (i; i > 0; i--) {
+		console.log(i);
+	}
+
+2d. if ((a && b)x = 0;) 
+	else if ((a || b) x = 1;) 
+	else (x = 2;)
+
+3. var letters ['A','B','C','D','E'];
+   var output = '';
+   var n = 4;
+   for (var line = 0; line < n; line ++){
+   	for (var col = 0; col <= line; col ++) {
+   		output = output + letters[col];
+   	}
+   	output + = '/n';
+   	}
+   }
+
+4. and2(a,b)
